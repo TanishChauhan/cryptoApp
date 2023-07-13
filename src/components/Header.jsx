16a4b11c287277d6d4a5f219a7d5aeb2131eda48
Tanish <tanish1472@gmail.com>
2023-07-13 12:04:50 +0530

    Created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Button,HStack} from "@chakra-ui/react";
+import ColorModeSwitcher from '../ColorModeSwitcher';
+// import { chakra } from '@chakra-ui/react';
+import {Link} from "react-router-dom";
+const Header = () => {
+  return (
+    <HStack p={'4'} shadow={'base'} bgColor={"blackAlpha.900"}>
+        <Button variant ={'unstyled'} color={'white'}>
+            <Link to="/" >Home</Link>
+        </Button>
+        <Button variant ={'unstyled'} color={'white'}>
+            <Link to="/exchanges" >Exchanges</Link>
+        </Button>
+        <Button variant ={'unstyled'} color={'white'}>
+            <Link to="/coins" >Coins</Link>
+        </Button>
+        
+        <ColorModeSwitcher/>
+    </HStack>
+    
+  )
+}
+
+export default Header
